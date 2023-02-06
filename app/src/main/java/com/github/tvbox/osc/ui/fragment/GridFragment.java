b46dd4ae202f08e96793e6ca265a658c35c2f6d2
Swifly <cyf783>
2023-02-06 11:21:31 +0800

    改个名

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
@@ -296,11 +296,11 @@ public class GridFragment extends BaseLazyFragment {
         showLoading();
         isLoad = false;
         scrollTop();
-        toggleFilterColor();
+        toggleFilterStatus();
         sourceViewModel.getList(sortData, page);
     }
 
-    private void toggleFilterColor() {
+    private void toggleFilterStatus() {
         if (sortData.filters != null && !sortData.filters.isEmpty()) {
             int count = sortData.filterSelectCount();
             EventBus.getDefault().post(new RefreshEvent(RefreshEvent.TYPE_FILTER_CHANGE, count));
